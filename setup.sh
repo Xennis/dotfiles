31124dacc598666d11cbe16686ec0899a93456cc
Xennis <code@xennis.org>
2018-12-03 14:26:37 +0100

    Quote bash arguments in setup script

--- setup.sh
+++ setup.sh
@@ -10,30 +10,30 @@
 
 print_info() {
   # Print output in purple
-  printf " %s\\n" "$1"
+  printf " %s\\n" "${1}"
 }
 
 print_success() {
   # Print output in green
-  printf "\\e[0;32m  [✔] %s\\e[0m\\n" "$1"
+  printf "\\e[0;32m  [✔] %s\\e[0m\\n" "${1}"
 }
 
 print_error() {
   # Print output in red
-  printf "\\e[0;31m  [✖] %s %s\\e[0m\\n" "$1" "$2"
+  printf "\\e[0;31m  [✖] %s %s\\e[0m\\n" "${1}" "${2}"
 }
 
 print_result() {
-  [ "$1" -eq 0 ] \
-    && print_success "$2" \
-    || print_error "$2"
+  [ "${1}" -eq 0 ] \
+    && print_success "${2}" \
+    || print_error "${2}"
 
-  [ "$3" == "true" ] && [ "$1" -ne 0 ] \
+  [ "${3}" == "true" ] && [ "${1}" -ne 0 ] \
     && exit
 }
 
 execute() {
-  $1 &> /dev/null
+  ${1} &> /dev/null
   print_result $? "${2:-$1}"
 }
 
@@ -43,7 +43,7 @@ execute() {
 ###############################################################################
 
 clone_repository() {
-  local dotfiles_dir=$1
+  local dotfiles_dir="${1}"
 
   if [ ! -d "${dotfiles_dir}" ]; then
       print_info "Clone repository"
@@ -52,12 +52,12 @@ clone_repository() {
 }
 
 move_existing_file() {
-  local dotfiles_backup_dir=$1
-  local file=$2
+  local dotfiles_backup_dir="${1}"
+  local file="${2}"
 
   if [ ! -d "${dotfiles_backup_dir}" ]; then
-    print_info "Create $dotfiles_backup_dir for backup"
-    mkdir -p "$dotfiles_backup_dir"
+    print_info "Create ${dotfiles_backup_dir} for backup"
+    mkdir -p "${dotfiles_backup_dir}"
   fi
 
   # Check file (or dir) exists and is not a link
@@ -68,29 +68,29 @@ move_existing_file() {
 }
 
 move_existing_files() {
-  local dotfiles_backup_dir=$1
+  local dotfiles_backup_dir="${1}"
   local files_to_symlink=("${@:2}")
 
   for i in "${files_to_symlink[@]}"; do
-    if [[ $i == .* ]]; then
-      targetFile=~/$i
+    if [[ "${i}" == .* ]]; then
+      targetFile="~/${i}"
     else
-      targetFile=~/.${i##*/}
+      targetFile="~/.${i##*/}"
     fi
-    move_existing_file "$dotfiles_backup_dir" "$targetFile"
+    move_existing_file "${dotfiles_backup_dir}" "${targetFile}"
   done
 }
 
 create_symbolic_link() {
-  local sourceFile=$1
-  local targetFile=$2
+  local sourceFile="${1}"
+  local targetFile="${2}"
 
-  if [ ! -e "$targetFile" ]; then
-    execute "ln -fs $sourceFile $targetFile" "$targetFile → $sourceFile"
-  elif [ "$(readlink "$targetFile")" == "$sourceFile" ]; then
-    print_success "$targetFile → $sourceFile"
+  if [ ! -e "${targetFile}" ]; then
+    execute "ln -fs ${sourceFile} ${targetFile}" "${targetFile} → ${sourceFile}"
+  elif [ "$(readlink "${targetFile}")" == "${sourceFile}" ]; then
+    print_success "${targetFile} → ${sourceFile}"
   else
-    print_error "$targetFile → $sourceFile"
+    print_error "${targetFile} → ${sourceFile}"
   fi
 }
 
@@ -98,13 +98,13 @@ create_symbolic_links() {
   local files_to_symlink=("$@")
 
   for i in "${files_to_symlink[@]}"; do
-    sourceFile="$(pwd)/$i"
-    if [[ $i == .* ]]; then
-      targetFile="$HOME/$i"
+    sourceFile="$(pwd)/${i}"
+    if [[ "${i}" == .* ]]; then
+      targetFile="${HOME}/${i}"
     else
-      targetFile="$HOME/.$(printf "%s" "$i" | sed "s/.*\/\(.*\)/\1/g")"
+      targetFile="${HOME}/.$(printf "%s" "${i}" | sed "s/.*\/\(.*\)/\1/g")"
     fi
-    create_symbolic_link "$sourceFile" "$targetFile"
+    create_symbolic_link "${sourceFile}" "${targetFile}"
   done
 }
 
@@ -113,12 +113,12 @@ create_symbolic_links() {
 # The script
 ###############################################################################
 
-DOTFILES_DIR="$HOME/dotfiles"
+DOTFILES_DIR="${HOME}/dotfiles"
 DOTFILES_BACKUP_DIR="${DOTFILES_DIR}_old"
 
 # Create directories for config files inside of subdirectories
-mkdir -p "$HOME/.config/Code/User"
-mkdir -p "$HOME/.config/spotify/Users/xen_nis-user"
+mkdir -p "${HOME}/.config/Code/User"
+mkdir -p "${HOME}/.config/spotify/Users/xen_nis-user"
 
 declare -a FILES_TO_SYMLINK=(
   '.config/awesome'
